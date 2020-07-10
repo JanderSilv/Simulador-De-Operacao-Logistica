21e532d8b915d82539a5fc40ae485245fc6a297b
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-07-09 22:15:38 -0300

    Pacot Intl

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
 import Routes from './src/routes';
 import { StepsProvider } from './src/contexts/steps';
+import { OperationReportProvider } from './src/contexts/operationReport';
 
 export default function App() {
     return (
-        <StepsProvider>
-            <Routes />
-        </StepsProvider>
+        <OperationReportProvider>
+            <StepsProvider>
+                <Routes />
+            </StepsProvider>
+        </OperationReportProvider>
     );
 }
